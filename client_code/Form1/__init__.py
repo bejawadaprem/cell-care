5cee48950015d8763832439a665c3a1176796ccf
bejawadaprem@gmail.com <bejawadaprem@gmail.com>
2023-10-12 04:53:58 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,5 +5,14 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    my_name='My Name is Prem Kumar'
+    date_ofbirth='july 1st'
+    alert(my_name)
+    alert(date_ofbirth)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
