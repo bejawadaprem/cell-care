23752bae2faf990d352ff3b7dddd5241941f1717
bejawadaprem@gmail.com <bejawadaprem@gmail.com>
2023-10-12 10:02:24 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -14,34 +14,34 @@ class Form1(Form1Template):
     
     
 
-  def text_box_1_pressed_enter(self, **event_args):
-    name=self.text_box_1.text
-    alert(name)
+ # def text_box_1_pressed_enter(self, **event_args):
+    #name=self.text_box_1.text
+    #alert(name)
 
   #def button_1_click(self, **event_args):
     #name=self.text_box_1.text
     #alert('Thank for your Feedback')
     #This method is called when the button is clicked"
 
-  def text_box_2_pressed_enter(self, **event_args):
-    mobile=self.text_box_2.text
-    alert(mobile)
+  #def text_box_2_pressed_enter(self, **event_args):
+    #mobile=self.text_box_2.text
+    #alert(mobile)
     """This method is called when the user presses Enter in this text box"""
 
-  def text_box_3_pressed_enter(self, **event_args):
-    model=self.text_box_3.text
-    alert(model)
+  #def text_box_3_pressed_enter(self, **event_args):
+    #model=self.text_box_3.text
+    #alert(model)
     """This method is called when the user presses Enter in this text box"""
 
-  def text_box_4_pressed_enter(self, **event_args):
-    address=self.text_box_4.text
-    alert(address)
+  #def text_box_4_pressed_enter(self, **event_args):
+    #address=self.text_box_4.text
+    #alert(address)
     """This method is called when the user presses Enter in this text box"""
 
-  def button_1_click(self, **event_args):
+  #def button_1_click(self, **event_args):
     #button=self.text_box_4.text
-    alert('Thanks for contacting us!')
-    anvil.server.call(name,mobile,model,address)
+    #alert('Thanks for contacting us!')
+    #anvil.server.call(name,mobile,model,address)
     """This method is called when the button is clicked"""
   
 
